93021268a1f524821190dae056562d9a4fc485ad
Furkan Salık <fsalik25@outlook.com>
2022-07-27 18:00:03 +0300

    Remove STATICFILES_DIRS constant.

--- polling_site/settings.py
+++ polling_site/settings.py
@@ -135,10 +135,6 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
